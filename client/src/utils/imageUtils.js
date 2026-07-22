20a29912db1af720b91443933b4c53613fd139d7
Akhilsaireddy2005 <akhiljaggavarapu@gmail.com>
2026-07-22 23:16:51 +0530

    18

--- client/src/utils/imageUtils.js
+++ client/src/utils/imageUtils.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_SERVER_URL || "https://lost-and-found-1-ryyr.onrender.com";
 
 export const getImageUrl = (imagePath) => {
   if (!imagePath) return null;
